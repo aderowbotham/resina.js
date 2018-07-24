e1533e9ce1bd71ffff08ee4a35ce83c2f44dc119
Ade Rowbotham <aderowbotham@gmail.com>
2018-07-24 16:05:56 +0100

    Version 1.1

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,28 +1,25 @@
-var frontendJS = [ 'resina.js' ];
-
-
-var closurePath = '/Users/ade/Sites/resina.js';
+var closurePath = "closure_compiler";  // must match local path of PARENT directory of 'build' folder, which contains the compiler.jar file
 
 module.exports = function(grunt) {
   // Do grunt-related things in here
 
   // load all grunt tasks
-  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+  require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
 
-  grunt.loadNpmTasks('grunt-closure-compiler');
+  grunt.loadNpmTasks("grunt-closure-compiler");
 
   grunt.initConfig({
 
-    'closure-compiler': {
+    "closure-compiler": {
       frontend: {
         closurePath: closurePath,
-        js: frontendJS,
-        jsOutputFile: 'resina-min.js',
+        js: [ "resina.js" ],
+        jsOutputFile: "resina-min.js",
         maxBuffer: 500,
         noreport: true,
         options: {
-          compilation_level: 'SIMPLE_OPTIMIZATIONS'  // options are WHITESPACE_ONLY, SIMPLE_OPTIMIZATIONS, ADVANCED_OPTIMIZATIONS
-          // language_in: 'ECMASCRIPT5_STRICT'
+          compilation_level: "SIMPLE_OPTIMIZATIONS"  // options are WHITESPACE_ONLY, SIMPLE_OPTIMIZATIONS, ADVANCED_OPTIMIZATIONS
+          // language_in: "ECMASCRIPT5_STRICT"
         }
       }
     },
@@ -30,8 +27,8 @@ module.exports = function(grunt) {
     watch: {
       /* watch and see if our javascript files change, or new packages are installed */
       js: {
-        files: ['resina.js'],
-        tasks: ['closure-compiler:frontend']
+        files: ["resina.js"],
+        tasks: ["closure-compiler:frontend"]
       },
 
     }
@@ -39,6 +36,6 @@ module.exports = function(grunt) {
   });
 
   // required default task
-  grunt.registerTask('default', 'watch');
+  grunt.registerTask("default", "watch");
 
 };
--- resina.js
+++ resina.js
@@ -4,54 +4,98 @@
 
 (function(){
 
-  'use strict';
+  "use strict";
 
-  var retinaIsSet = false;
+  window.resina = (function(){
 
-  var settings = {
-    'minPixelRatio': 1.5,
-    'minPixelWidth': 800, //NOTE zero means this now applies to all images if display is retina, regardless of screen size
-    'className': 'hd',
-    'suffix' : '@2x'
-  };
+    var retinaIsSet = false;
+    var firstRun = true;
 
-  var init = function(){
-    window.addEventListener('resize', doTest, false);
-    doTest();
-  };
 
-  var doTest = function(){
-    var isRetina = window.devicePixelRatio >= settings.minPixelRatio && Math.max(window.screen.availHeight,window.screen.availWidth) >= settings.minPixelWidth;
+    var settings = {
+      minPixelRatio: 1.5,
+      minPixelWidth: 800, // @NOTE zero means this now applies to all images if display is retina, regardless of screen size
+      className: "hd",
+      suffix : "@2x"
+    };
 
-    if(isRetina && !retinaIsSet){
-      retinaIsSet = true;
-      window.removeEventListener('resize', doTest, false);
 
-      applyRetinaAssets();
+
+    function init(_settings_){
+      if(_settings_){
+        for (var property in settings) {
+          if (settings.hasOwnProperty(property) && typeof _settings_[property] !== typeof undefined) {
+            settings[property] = _settings_[property];
+          }
+        }
+      }
+
+      window.addEventListener("resize", doTest, false);
+      doTest();
     }
-  };
 
-  var applyRetinaAssets = function(){
-    var images = top.document.getElementsByTagName('img');
 
-    for(var i=0; i<images.length; i++){
-      if(hasClass(images[i],settings.className)){
-        images[i].src = images[i].src.replace(/(\.[\w\d_-]+)$/i, settings.suffix+'$1');
+
+    function doTest(){
+      var isRetina = window.devicePixelRatio >= settings.minPixelRatio && Math.max(window.screen.availHeight,window.screen.availWidth) >= settings.minPixelWidth;
+
+      if(isRetina && !retinaIsSet){
+        retinaIsSet = true;
+        window.removeEventListener("resize", doTest, false);
+
+        return _parseImages(true);
+      }
+
+      if(firstRun){
+        _parseImages(false);
+        firstRun = false;
       }
     }
-  };
 
-  function hasClass(element, cls) {
-    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
-  }
 
-  // run as soon as possible
-  document.ready = init;
 
-  // uncomment to expose init method
-  // e.g. to pass the settings object as an argument
-  // return {
-  //   init: init
-  // };
+    function _parseImages(retinaMode){
+      var images = top.document.getElementsByTagName('img');
+
+      for(var i=0; i<images.length; i++){
+        if(_hasClass(images[i], settings.className)){
+
+          var originalSource = images[i].getAttribute("data-src") || images[i].src;
+          if(!originalSource){
+            return;
+          }
+
+          if(retinaMode){
+            images[i].src = originalSource.replace(/(\.[\w\d_-]+)$/i, settings.suffix+'$1');
+          } else {
+            // support images presented with a data-src attribute but no src attribute that are NOT being retinized
+            // only happens on first run
+            if(!images[i].src){
+              images[i].src = originalSource;
+            }
+          }
+
+        }
+      }
+    }
+
+
+    function _hasClass(element, cls) {
+      return (" " + element.className + " ").indexOf(" " + cls + " ") !== -1;
+    }
+
+
+    // uncomment to expose init method
+    // e.g. to pass the settings object as an argument
+    // return {
+    //   init: init
+    // };
+
+    // otherwise self-init
+    document.ready = init;
+
+
+  })();
+
 
 })();
